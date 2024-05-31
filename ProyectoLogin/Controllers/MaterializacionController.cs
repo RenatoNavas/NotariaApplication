d4cb7aee7c581a71f610c5f3018361f2398cd435
Fausto Rodrigo Vizuete Morales <67701116+favizuete@users.noreply.github.com>
2024-05-30 23:54:25 -0500

    Materializacion

---
+++ ProyectoLogin/Controllers/MaterializacionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoLogin.Controllers
+{
+    public class MaterializacionController : Controller
+    {
+        public IActionResult Materializacion()
+        {
+            return View();
+        }
+    }
+}
