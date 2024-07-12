aedfbaacd1a7c2e3fe3d7c2424c9d810c367c473
Fausto Rodrigo Vizuete Morales <67701116+favizuete@users.noreply.github.com>
2024-07-11 23:49:24 -0500

    Inicio-CopiaArchivos

---
+++ ProyectoLogin/Controllers/CopiaArchivosController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoLogin.Controllers
+{
+    public class CopiaArchivosController : Controller
+    {
+        // Acción que renderiza la vista de Copia de Archivos
+        public IActionResult CopiaArchivos()
+        {
+            return View();
+        }
+    }
+}
